d1ee7bf976a34e4276874712877695241568913a
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-24 22:15:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,5 +9,11 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  
+  nds = directors_database
+  director_index = 0
+  movie_index = 0
+  while movie_index < nds[director_index][:movies].length do
+    movie_title = nds[director_index][:movies][movie_index][:title]
+    puts movie_title
+    movie_index += 1
 end
